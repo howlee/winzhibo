d0fcef680a4840dc18942c3e68c9261a241ffa39
howlee <112477130@qq.com>
2019-07-05 11:13:05 +0800

    修复录像翻页bug

--- resources/views/pc/video/list_page_cell.blade.php
+++ resources/views/pc/video/list_page_cell.blade.php
@@ -14,27 +14,27 @@
         $index = $index <= 1 ? 2 : $index;
     ?>
     @if($lastPage > 7 && $curPage != 1)
-        <a href="/live/videos/index{{$curPage - 1}}.html" class="a1">上一页</a>
+        <a href="/video/index{{$curPage - 1}}.html" class="a1">上一页</a>
     @endif
     @if($curPage == 1)
         <strong>1</strong>
     @else
-        <a href="/live/videos ">1 @if($index > 2)...@endif</a>
+        <a href="/video/">1 @if($index > 2)...@endif</a>
     @endif
     @for($f_index = 0; $f_index < $showBtn; $f_index++)
         @continue($index >= $lastPage)
         @if($curPage == $index)
             <strong>{{$index}}</strong>
         @else
-            <a href="/live/videos/index{{$index}}.html" >{{$index}}</a>
+            <a href="/video/index{{$index}}.html" >{{$index}}</a>
         @endif
         <?php $index++; ?>
     @endfor
     @if($curPage == $lastPage)
         <strong>{{$lastPage}}</strong>
     @else
-        <a href='/live/videos/index{{$lastPage}}.html' >@if($index < $lastPage)...@endif{{$lastPage}}</a>
+        <a href='/video/index{{$lastPage}}.html' >@if($index < $lastPage)...@endif{{$lastPage}}</a>
     @endif
-    @if ($lastPage > 7 && $curPage != $lastPage) <a href="/live/videos/index{{$curPage + 1}}.html" class="a1">下一页</a> @endif
+    @if ($lastPage > 7 && $curPage != $lastPage) <a href="/video/index{{$curPage + 1}}.html" class="a1">下一页</a> @endif
     </div>
 @endif
